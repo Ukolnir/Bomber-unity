137af9719dace84ca4dab397e4c6277b3f107d4a
Ukolnir <metallwind@gmail.com>
2021-07-24 14:18:08 +0300

    add player&enemy animations

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,34 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+
     public float speed;
-    private bool isMove = false;
+    public Animator animator;
+    public AIPath aiPath;
 
-    public GameObject player; 
-
-    // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (isMove){
-          rb2d.velocity = default;
-          isMove = false;
-        }
+        Vector2 velocity = aiPath.desiredVelocity.normalized;
+
+        animator.SetFloat("Horizontal", velocity.x);
+        animator.SetFloat("Vertical", velocity.y);
+        animator.SetFloat("Speed", Mathf.Approximately(aiPath.desiredVelocity.sqrMagnitude, 0f) ? 0 : 1);
     }
 
     void FixedUpdate() {
-        Vector3 direct = (player.transform.position - transform.position).normalized;
-        Debug.Log(player.transform.position);
-        rb2d.velocity = new Vector2(direct.x, direct.y) * speed;
-        isMove = true;
+     
     }
 }
